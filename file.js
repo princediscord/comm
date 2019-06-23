6f5de9544194d52f580a0d7bcc9dc10f35573dca
princediscord <51452946+princediscord@users.noreply.github.com>
2019-06-23 12:18:31 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,13 @@
+```const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("592093089729085472")
+setInterval(function() {
+channel.send(`!spam`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
